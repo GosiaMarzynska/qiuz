de1e2477f99847f2238d00b3a0d2f99fa614f51a
Malgorzata Marzynska <ma.marzynska@gmail.com>
2023-04-05 20:21:20 +0200

    add prepare game setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,40 @@
-import React, {useContext} from 'react';
+import React, { useContext, useState } from 'react';
 import { AuthContext } from './context/auth-context';
-import Welcome from './components/Welcome'
+import Welcome from './components/Welcome';
 import StartGame from './components/StartGame';
 
 function App() {
-  const authContext = useContext(AuthContext);
+	const authContext = useContext(AuthContext);
+	const [difficulty, setDifficulty] = useState('medium');
+	const [questionsAmount, setQuestionsAmount] = useState(5);
+	const [isGameSet, setIsGameSet] = useState(false);
 
-  let content = <Welcome />
+	const radioClickHandler = event => {
+		setDifficulty(event.currentTarget.value);
+	};
 
-  if (authContext.isAuth){
-    content = <StartGame/>
-  }
+	const questionsAmountHandler = event => {
+		setQuestionsAmount(event.currentTarget.value);
+	};
+  
+  let url;
+ const submitHandler=(event)=> {
+  event.preventDefault();
+  url = `https://the-trivia-api.com/api/questions?limit=${questionsAmount}&region=PL&difficulty=${difficulty}`;
+  setIsGameSet(true)
+  console.log(url);
 
-  return (
-    <div className="App">
-      {content}
-    </div>
-  )
+ }
+
+
+
+	let content = <Welcome />;
+
+	if (authContext.isAuth) {
+		content = <StartGame onRadioChange={radioClickHandler} onQuestionsAmountChange={questionsAmountHandler} difficulty={difficulty} prepareGame={submitHandler}/>;
+	}
+
+	return <div className='App'>{content}</div>;
 }
 
-export default App
+export default App;
--- src/components/StartGame.jsx
+++ src/components/StartGame.jsx
@@ -2,25 +2,20 @@ import React, { useContext, useState } from 'react';
 import { AuthContext } from '../context/auth-context';
 import classes from './StartGame.module.css';
 
-export default function StartGame() {
+export default function StartGame({onRadioChange, onQuestionsAmountChange, difficulty, prepareGame}) {
 	const authContext = useContext(AuthContext);
-    const [difficulty, setDifficulty]= useState('medium');
-    const [questionsAmount, setQuestionsAmount] = useState(5)
+
 
 	const name = authContext.name;
 
-    const radioClickHandler = event => {setDifficulty(event.currentTarget.value)
-    console.log(difficulty)}
     const isRadioSelect = (selectDifficulty) => selectDifficulty === difficulty;
 
-const questionsAmountHandler = event => {setQuestionsAmount(event.currentTarget.value); console.log(questionsAmount)}
-	const prepareGame = event => {
-		event.preventDefault();
-	};
+
+
 
 	return (
 		<div className={classes.card}>
-			<h2 className={classes.title}>Hi {name ? name : 'Mateusz'}!</h2>
+			<h2 className={classes.title}>Hi {name ? name : 'User'}!</h2>
 			<h2>Your game will be ready soon!</h2>
             <br/>
 			<form onSubmit={prepareGame} className={classes.form}>
@@ -28,17 +23,17 @@ const questionsAmountHandler = event => {setQuestionsAmount(event.currentTarget.
                     <p className={classes.label}>Select Difficulty</p>
                     <div className={classes.radio}>
 		        
-					<input className={classes['radio-input']} type='radio' name='difficulty' id='easy' value='easy' checked={isRadioSelect('easy')} onChange={radioClickHandler}/>
+					<input className={classes['radio-input']} type='radio' name='difficulty' id='easy' value='easy' checked={isRadioSelect('easy')} onChange={onRadioChange}/>
 					<label className={classes['radio-label']} htmlFor='easy'>
 						Easy
 					</label>
 
-					<input className={classes['radio-input']} type='radio' name='difficulty' id='medium' value='medium' checked={isRadioSelect('medium')} onChange={radioClickHandler}/>
+					<input className={classes['radio-input']} type='radio' name='difficulty' id='medium' value='medium' checked={isRadioSelect('medium')} onChange={onRadioChange}/>
 					<label className={classes['radio-label']} htmlFor='medium'>
 						Medium 
 					</label>
 
-					<input className={classes['radio-input']} type='radio' name='difficulty' id='hard' value='hard' checked={isRadioSelect('hard')} onChange={radioClickHandler}/>
+					<input className={classes['radio-input']} type='radio' name='difficulty' id='hard' value='hard' checked={isRadioSelect('hard')} onChange={onRadioChange}/>
 					<label className={classes['radio-label']} htmlFor='hard'>
 						Hard
 					</label>
@@ -46,7 +41,7 @@ const questionsAmountHandler = event => {setQuestionsAmount(event.currentTarget.
                 </div>
 				<p>
 					<label className={classes.label} htmlFor='questionsAmount'>How many questions?</label>
-					<input className={classes['form-input']}type='number' id='questionsAmount' name='questionsAmount' min='1' max='20' defaultValue='5' onChange={questionsAmountHandler}/>
+					<input className={classes['form-input']}type='number' id='questionsAmount' name='questionsAmount' min='1' max='20' defaultValue='5' onChange={onQuestionsAmountChange}/>
 				</p>
 				<div className={classes.actions}>
 					<button type='submit'>Start Game</button>
